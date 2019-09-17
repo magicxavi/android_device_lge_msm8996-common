2dcfe41c176bcc1114760f6e5afb011c5a4b15a1
dom133 <dom133_pl@protonmail.com>
2019-09-17 17:27:56 +0200

    Revert "msm8996-common: Re-enable PRODUCT_BOOT_JARS"
    This reverts commit a9777a259b75b649d9065c0d9bc2dd2eb94f78f6.

--- msm8996.mk
+++ msm8996.mk
@@ -408,11 +408,11 @@ PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 	
 # Telephony
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # USB
 PRODUCT_PACKAGES += \
