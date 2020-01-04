cf2752792efa173f5c5bdbe086b11cb7d2dea805
Michael Bardoň <wolfovelp@gmail.com>
2020-01-04 15:07:25 +0100

    Revert "msm8996-common: Update CNE and DPM from LA.UM.8.6.r1-01900-89xx.0"
    This reverts commit e0986a9146fd5eda38ee07dd8fd10a191e4fbf9e.

--- msm8996.mk
+++ msm8996.mk
@@ -359,8 +359,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
+    liblge \
     libprotobuf-cpp-full \
     librmnetctl
 
